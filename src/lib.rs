8dce493ea8e2d45468e2fc96993c7dce52ff1107
defiborg <157887165+defiborg@users.noreply.github.com>
2025-05-23 10:00:06 +0200

    Update lib.rs
    Signed-off-by: defiborg <157887165+defiborg@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
 pub mod plasma;
+
+use plasma::{PoolHeader, plasma_amm::Amm as PlasmaAmmState};
+
+#[derive(Debug, Copy, Clone, BorshDeserialize, BorshSerialize)]
+#[repr(C)]
+pub struct PoolAccount {
+    pub header: PoolHeader,
+    pub amm: PlasmaAmmState,
+}
